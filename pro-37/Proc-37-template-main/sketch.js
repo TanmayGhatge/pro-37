ee756c31d0ed37abc34f20ddc6833f185c816dec
TanmayGhatge <75845981+TanmayGhatge@users.noreply.github.com>
2021-05-25 14:03:22 +0530

    Add files via upload

---
+++ pro-37/Proc-37-template-main/sketch.js
@@ -0,0 +1,29 @@
+var canvas, backgroundImage;
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-50,displayHeight-30);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
